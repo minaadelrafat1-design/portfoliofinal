a1ffbbf18da33c68e131578a85d0ed0f5e845f36
minaadelrafat1-design <minaadelrafat1@gmail.com>
2026-07-25 20:50:49 +0300

    Add files via upload

--- portfolio3-secured/project/src/pages/AdminApp.tsx
+++ portfolio3-secured/project/src/pages/AdminApp.tsx
@@ -5,7 +5,6 @@ import { AdminLayout, type AdminTab } from '@/components/admin/AdminLayout';
 import { AdminOverview } from '@/components/admin/AdminOverview';
 import { AdminProjects } from '@/components/admin/AdminProjects';
 import { AdminProfile } from '@/components/admin/AdminProfile';
-import { AdminSkills } from '@/components/admin/AdminSkills';
 import { AdminServices } from '@/components/admin/AdminServices';
 import { AdminMessages } from '@/components/admin/AdminMessages';
 import { AdminOrders } from '@/components/admin/AdminOrders';
@@ -25,7 +24,6 @@ export function AdminApp() {
       {tab === 'overview' && <AdminOverview onNavigate={setTab} />}
       {tab === 'projects' && <AdminProjects />}
       {tab === 'profile' && <AdminProfile />}
-      {tab === 'skills' && <AdminSkills />}
       {tab === 'services' && <AdminServices />}
       {tab === 'content' && <AdminContent />}
       {tab === 'messages' && <AdminMessages onReadChange={setUnreadMessages} />}
